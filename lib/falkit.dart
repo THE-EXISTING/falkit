b84544977a83dda28f5f42237736198c381132e6
Nonthawit Doungsodsri <nonthawit.kub@gmail.com>
2024-05-17 22:01:25 +0700

    first commit

---
+++ lib/falkit.dart
@@ -0,0 +1,9 @@
+library falkit;
+
+export 'package:gap/gap.dart';
+export 'package:shimmer/shimmer.dart';
+export 'package:flutter_svg/flutter_svg.dart';
+export 'package:extended_image/extended_image.dart';
+
+export 'widgets/widgets.dart';
+export 'extensions/extensions.dart';
---
+++ lib/lib.dart
@@ -0,0 +1,14 @@
+export 'dart:convert';
+export 'dart:async';
+export 'dart:math';
+
+export 'package:faltool/faltool.dart' hide Notification;
+export 'package:flutter/material.dart';
+export 'package:flutter_svg/flutter_svg.dart';
+export 'package:gap/gap.dart';
+export 'package:shimmer/shimmer.dart';
+export 'package:extended_image/extended_image.dart';
+
+export 'widgets/widgets.dart';
+export 'extensions/extensions.dart';
+
