196881455a5f1dc676cb1315050d7d47f50c4e64
Nonthawit Doungsodsri <nonthawit.kub@gmail.com>
2024-05-17 22:24:13 +0700

    [+] hide some class

--- lib/falkit.dart
+++ lib/falkit.dart
@@ -3,7 +3,8 @@ library falkit;
 export 'package:gap/gap.dart';
 export 'package:shimmer/shimmer.dart';
 export 'package:flutter_svg/flutter_svg.dart';
-export 'package:extended_image/extended_image.dart';
+export 'package:extended_image/extended_image.dart'
+    hide MultipartFile, BaseRequest;
 
 export 'widgets/widgets.dart';
 export 'extensions/extensions.dart';
