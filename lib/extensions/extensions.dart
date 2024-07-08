0d7ca8c39b021783791fb6774eaad827c89cfc7d
Nonthawit Doungsodsri <nonthawit.kub@gmail.com>
2024-07-08 15:29:18 +0700

    [+] update extension

--- lib/extensions/extensions.dart
+++ lib/extensions/extensions.dart
@@ -1,3 +1,27 @@
+import '../lib.dart';
+
 export 'icons_extensions.dart';
 export 'images_extensions.dart';
+
+extension WidgetStateExtension on WidgetState? {
+  bool get isNormal => this == null;
+  bool get isHovered => this == WidgetState.hovered;
+  bool get isFocused => this == WidgetState.focused;
+  bool get isPressed => this == WidgetState.pressed;
+  bool get isDragged => this == WidgetState.dragged;
+  bool get isSelected => this == WidgetState.selected;
+  bool get isScrolledUnder => this == WidgetState.scrolledUnder;
+  bool get isDisabled => this == WidgetState.disabled;
+  bool get isError => this == WidgetState.error;
+
+  bool get isNotNormal => !isNormal;
+  bool get isNotHovered => !isHovered;
+  bool get isNotFocused => !isFocused;
+  bool get isNotPressed => !isPressed;
+  bool get isNotDragged => !isDragged;
+  bool get isNotSelected => !isSelected;
+  bool get isNotScrolledUnder => !isScrolledUnder;
+  bool get isNotDisabled => !isDisabled;
+  bool get isNotError => !isError;
+}
 
